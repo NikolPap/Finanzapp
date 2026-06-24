97abe9663641c9b405e66dce5ef7ff025d278bdb
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-06-24 13:15:04 +0200

    first commit

---
+++ src/main/java/com/finanzmanager/Finanzapp/FinanzappApplication.java
@@ -0,0 +1,13 @@
+package com.finanzmanager.Finanzapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzappApplication.class, args);
+	}
+
+}
