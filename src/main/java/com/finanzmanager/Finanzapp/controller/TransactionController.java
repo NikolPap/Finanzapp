9e05026a7dd1074a7c59ecfe2792465798c9531f
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-06-25 22:37:32 +0200

    Refactor TransactionController to return ResponseEntity for transaction retrieval methods

--- src/main/java/com/finanzmanager/Finanzapp/controller/TransactionController.java
+++ src/main/java/com/finanzmanager/Finanzapp/controller/TransactionController.java
@@ -2,12 +2,14 @@ package com.finanzmanager.Finanzapp.controller;
 
 import com.finanzmanager.Finanzapp.model.Transaction;
 import com.finanzmanager.Finanzapp.service.TransactionService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/transaction")
+@SuppressWarnings("java:S4684")
 public class TransactionController {
     private final TransactionService service;
     public TransactionController(TransactionService service) {
@@ -15,13 +17,30 @@ public class TransactionController {
     }
 
     @GetMapping
-    public List<Transaction> getAllTransactions() {
-        return service.getAll();
+    public ResponseEntity<List<Transaction>> getAllTransactions() {
+        List<Transaction> transactions = service.getAll();
+        return ResponseEntity.ok(transactions);
+
     }
 
     @PostMapping
     public Transaction createTransaction(@RequestBody Transaction transaction) {
         return service.save(transaction);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> getById(@PathVariable Long id) {
+        Transaction transaction = service.getById(id);
+        if (transaction != null) {
+            return ResponseEntity.ok(transaction);
+        }else  {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/search")
+    public List<Transaction> searchByTitle(@RequestParam String title) {
+        return service.searchByTitle(title);
     }
 
 }
