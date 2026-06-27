ce10e02a219ce58afcd7ed334277d09125e27fd9
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-06-27 15:42:43 +0200

    Add validation annotations to the Transaction class

--- src/main/java/com/finanzmanager/Finanzapp/model/Transaction.java
+++ src/main/java/com/finanzmanager/Finanzapp/model/Transaction.java
@@ -1,9 +1,14 @@
 package com.finanzmanager.Finanzapp.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -15,8 +20,15 @@ public class Transaction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Title darf nicht leer sein")
     private String title;
+
+    @Positive(message = "Betrag muss positiv sein")
     private Double amount;
+
+    @NotNull(message = "Datum darf nicht null sein")
+    @PastOrPresent(message = "Datum darf nicht in der Zukunft liegen")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
 
 
