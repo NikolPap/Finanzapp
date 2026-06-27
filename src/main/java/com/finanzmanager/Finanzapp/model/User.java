b13a2dfadf585d3250db8434bb249742a4d7d5ed
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-06-27 22:46:38 +0200

    add User class with user management attributes

---
+++ src/main/java/com/finanzmanager/Finanzapp/model/User.java
@@ -0,0 +1,40 @@
+package com.finanzmanager.Finanzapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    private  String firstName;
+    private String lastName;
+
+    private  Boolean isActive = true;
+
+    private LocalDate createAt;
+    private LocalDate updateAt;
+
+
+}
