85a85cec1c4389660572d5e49426c2f95eae32d1
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-06-28 21:11:28 +0200

    Add getTransactionsBetweenDates method to UserService

--- src/main/java/com/finanzmanager/Finanzapp/repository/TransactionRepository.java
+++ src/main/java/com/finanzmanager/Finanzapp/repository/TransactionRepository.java
@@ -5,9 +5,11 @@ import com.finanzmanager.Finanzapp.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByDateBetween(LocalDate start, LocalDate end);
     List<Transaction> findByTitleContainingIgnoreCase(String title);
 }
