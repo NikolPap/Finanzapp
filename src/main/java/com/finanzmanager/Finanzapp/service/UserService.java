0cae4d72c84d906927961475d62670b92a9c3cdd
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-06-28 21:11:50 +0200

    Implement UserService class with method to retrieve user by username

--- src/main/java/com/finanzmanager/Finanzapp/service/UserService.java
+++ src/main/java/com/finanzmanager/Finanzapp/service/UserService.java
@@ -1,4 +1,19 @@
 package com.finanzmanager.Finanzapp.service;
 
+import com.finanzmanager.Finanzapp.model.User;
+import com.finanzmanager.Finanzapp.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<User> getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
 }
