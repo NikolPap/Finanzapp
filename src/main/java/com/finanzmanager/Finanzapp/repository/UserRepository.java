172c38a6428e3a74bd5d6653315d8f6fafe46fd4
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-06-28 21:11:43 +0200

    Add UserRepository interface for user data access

---
+++ src/main/java/com/finanzmanager/Finanzapp/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.finanzmanager.Finanzapp.repository;
+
+import com.finanzmanager.Finanzapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User>  findByEmail(String email);
+    Boolean existsByEmail(String email);
+}
