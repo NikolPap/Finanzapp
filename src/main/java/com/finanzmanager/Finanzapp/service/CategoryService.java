490f55bc4fb56a6d7335ceb7d7865634b2609c07
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-06-28 23:17:46 +0200

    Implementiere CategoryService mit CRUD-Methoden für Kategorien

---
+++ src/main/java/com/finanzmanager/Finanzapp/service/CategoryService.java
@@ -0,0 +1,48 @@
+package com.finanzmanager.Finanzapp.service;
+
+import com.finanzmanager.Finanzapp.model.Category;
+import com.finanzmanager.Finanzapp.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    //create
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    //Read alle
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    //read nach Id
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).orElseThrow(()-> new RuntimeException("kategorie mit ID " + id + " nicht gefunden"));
+    }
+
+    //update
+    public Category updateCategory(Long id, Category updatedCategory) {
+      Category existngCategory = getCategoryById(id);
+
+      existngCategory.setName(updatedCategory.getName());
+      existngCategory.setType(updatedCategory.getType());
+      return categoryRepository.save(existngCategory);
+    }
+
+    //delete
+
+    public void deleteCategory(Long id) {
+        if (!categoryRepository.existsById(id)) {
+            throw new RuntimeException("kategorie " + id + " nicht gefunden");
+        }else {
+            categoryRepository.deleteById(id);
+        }
+    }
+}
