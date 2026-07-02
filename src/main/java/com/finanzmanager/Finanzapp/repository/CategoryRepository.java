3c132a7084d71f72c3774eb69892e038e37e7784
Nikoleta Papalazarou <150321195+NikolPap@users.noreply.github.com>
2026-07-02 13:15:24 +0200

    feat: erweitere CategoryRepository um Methoden zur Suche nach Kategorie-Typ und Namen

--- src/main/java/com/finanzmanager/Finanzapp/repository/CategoryRepository.java
+++ src/main/java/com/finanzmanager/Finanzapp/repository/CategoryRepository.java
@@ -1,7 +1,19 @@
 package com.finanzmanager.Finanzapp.repository;
 
 import com.finanzmanager.Finanzapp.model.Category;
+import com.finanzmanager.Finanzapp.model.CategoryType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    //Query-Methode
+    List<Category> findByType(CategoryType type);
+
+    //Query mit JPQL
+    @Query("select c from Category c where c.name like  %:keyword%")
+    List<Category> searchByName(@Param("keyword") String keyword);
+
 }
